fbb09ffd83579f7f71c480e9572397acfd93e692
=Eneyda <eneyda_13@hotmail.com>
2023-01-14 22:10:00 -0600

    se agrega modificacion del proyecto calculadora

--- CaluladoraEnyda.py
+++ CaluladoraEnyda.py
@@ -4,39 +4,39 @@ import math
 
 # Sumar
 def sumar(valorUno, valorDos):
-    return valorUno + valorDos
+    return int(valorUno) + int(valorDos)
 
 
 # Restar
 def restar(valorUno, valorDos):
-    return valorUno - valorDos
+    return int(valorUno) - int(valorDos)
 
 
 # Multiplicación
 
 def multiplicar(valorUno, valorDos):
-    return valorUno * valorDos
+    return int(valorUno) * int(valorDos)
 
 
 # Division
 
 def dividir(valorUno, valorDos):
-    return valorUno / valorDos
+    return int(valorUno) / int(valorDos)
 
 
 # Raiz
 
 def raiz(valor1):
-    return math.sqrt(valor1)
+    return math.sqrt(int(valor1))
 
 
 # Exponente
 
 def exponente(base1, exponente):
-    return math.pow(base1, exponente)
+    return math.pow(int(base1), int(exponente))
 
 
 # Seno
 
 def seno(valor1):
-    return numpy.sin(valor1)
+    return numpy.sin(int(valor1))
--- Menu.py
+++ Menu.py
@@ -1,19 +1,25 @@
+from procesamiento import operaciones, obtenerResultado
 
 
 def calculadora():
     programa = True
     while programa:
-        print("Hello")
-        print("Agrega el Primer Numero")
-        numeroUno = input()
-
-        print("Agrega el Segundo Numero")
-        numeroDos = input()
+        operaciones()
         opcion = input()
-        if opcion == "11":
-            print("Misma Opcion")
-        if opcion == "12":
-            print("Salido")
+        obtenerResultado(opcion)
+        print("Para Continuar con la Caludadora cualquier otro numero")
+        print("9. Repetir Operación")
+        print("0. Salir")
+        segundaOpcion = input()
+        if segundaOpcion == "9":
+            print("Usted Quiere Repetir la Misma Opcion")
+            obtenerResultado(opcion)
+            print("Para Continuar con la Caludadora cualquier otro numero")
+            print("9. Repetir Operación")
+            print("0. Salir")
+            segundaOpcion = input()
+        if segundaOpcion == "0":
+            print("Gracias por usuar la Calculadora")
             programa = False
 
 
